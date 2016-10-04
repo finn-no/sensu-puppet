72c4da3cbe88ce91f4b23433c98565a566ba3b52
Kenneth Rørvik <kenneth.rorvik@finn.no>
2016-10-04 13:53:04 +0200

    come on, lets let owners edit stuff.... change default file modes

--- manifests/init.pp
+++ manifests/init.pp
@@ -528,8 +528,8 @@ class sensu (
       $conf_dir = "${etc_dir}/conf.d"
       $user = 'sensu'
       $group = 'sensu'
-      $dir_mode = '0777'
-      $file_mode = '0444'
+      $dir_mode = '0755'
+      $file_mode = '0644'
     }
 
     'windows': {
